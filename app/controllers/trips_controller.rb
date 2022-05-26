db1aceb4ec7008dec1076964c6b3bba42ce1bc30
Julia Thomas <juliathomas@outlook.com>
2022-05-26 21:32:41 +0100

    added validations and controller for Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,21 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+
+  def index
+  end
+
+  def destroy
+  end
+
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ApplicationRecord
+  validates :country, :region, :budget, :start_date, :activity_type, :duration, presence: true
+
+  belongs_to :user
 end
