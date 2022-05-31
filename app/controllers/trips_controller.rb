b4c7e6419b168f2d8add41959f5caa9f839b47f2
Julia Thomas <juliathomas@outlook.com>
2022-05-31 20:23:32 +0100

    added methods to trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,11 +1,10 @@
 class TripsController < ApplicationController
-
   def new
     @trip = Trip.new
   end
 
   def create
-    @trip = Trip.new(strong_params)
+    @trip = Trip.new(trip_params)
     @trip.user = current_user
     if trip.save!
       redirect_to account_path
@@ -15,14 +14,27 @@ class TripsController < ApplicationController
   end
 
   def update
+    @trip = Trip.find(params[:id])
+    if @trip.update
+      redirect_to account_path
+    else
+      render :edit
+    end
   end
 
-
   def index
+    @trip = Trip.all.sort_by(&:start_date)
   end
 
   def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to account_path
   end
 
+  private
 
+  def trip_params
+    params.require(:trip).permit(:country, :region, :budget, :start_date, :duration, :activity_type, :user_id)
+  end
 end
